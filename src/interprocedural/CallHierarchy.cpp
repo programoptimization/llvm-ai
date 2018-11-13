02cd9a0e57a0a95c23cba16625ff3a1b3df9f6f4
Denis Blank <denis.blank@tum.de>
2018-11-13 17:14:26 +0100

    Implement call hierarchy windowing properly

--- src/interprocedural/CallHierarchy.cpp
+++ src/interprocedural/CallHierarchy.cpp
@@ -1,21 +1,25 @@
 
 #include "CallHierarchy.h"
 #include "Hash.h"
+#include <algorithm>
 
-static const size_t CSA_DEPTH = 1;
+static std::size_t const callStringDepth = 1;
 
 namespace pcpo {
+bool CallHierarchy::isInMainFunction() const { return callInsts.empty(); }
+
 CallHierarchy CallHierarchy::append(llvm::CallInst *callInst) const {
   auto newCallInsts = callInsts;
   newCallInsts.push_back(callInst);
-  if (newCallInsts.size() > CSA_DEPTH) {
-    newCallInsts.pop_front();
-  }
-  return CallHierarchy(mainFunction, newCallInsts);
+
+  auto newOffset = std::max(std::int64_t(0), std::int64_t(newCallInsts.size()) -
+                                                 std::int64_t(callStringDepth));
+
+  return CallHierarchy(mainFunction, std::move(newCallInsts), newOffset);
 }
 
 llvm::Function *CallHierarchy::getCurrentFunction() const {
-  if (callInsts.empty()) {
+  if (isInMainFunction()) {
     // If we are in the main function return it
     return mainFunction;
   } else {
@@ -25,7 +29,43 @@ llvm::Function *CallHierarchy::getCurrentFunction() const {
 }
 
 bool CallHierarchy::operator==(CallHierarchy const &other) const {
-  return (mainFunction == other.mainFunction) && (callInsts == other.callInsts);
+  if (isInMainFunction() && other.isInMainFunction()) {
+    // If the call hierarchies are empty just compare the main functions
+    return mainFunction == other.mainFunction;
+  }
+
+  auto const begin = callInstructionsBegin();
+  auto const end = callInstructionsEnd();
+  auto const size = std::distance(begin, end);
+
+  auto const otherBegin = other.callInstructionsBegin();
+  auto const otherEnd = other.callInstructionsEnd();
+  auto const otherSize = std::distance(otherBegin, otherEnd);
+
+  /// The hierarchy depth of both hierarchies doesn't match
+  if (size != otherSize) {
+    return false;
+  }
+
+  /// Check whether both hierarchies match
+  return std::equal(begin, end, otherBegin);
+}
+
+CallHierarchy::CallInstructions::const_iterator
+CallHierarchy::callInstructionsBegin() const {
+  if (offset == 0) {
+    return callInsts.begin();
+  } else {
+    // Move the iterator to the current offset
+    auto itr = callInsts.begin();
+    std::advance(itr, offset);
+    return itr;
+  }
+}
+
+CallHierarchy::CallInstructions::const_iterator
+CallHierarchy::callInstructionsEnd() const {
+  return callInsts.end();
 }
 
 llvm::CallInst *CallHierarchy::getLastCallInstruction() const {
@@ -38,6 +78,12 @@ llvm::CallInst *CallHierarchy::getLastCallInstruction() const {
 } // namespace pcpo
 
 size_t std::hash<pcpo::CallHierarchy>::
-operator()(pcpo::CallHierarchy const& hierarchy) const {
-  return 0; //  pcpo::multiHash(hierarchy.mainFunction, hierarchy.callInsts);
+operator()(pcpo::CallHierarchy const &hierarchy) const {
+  if (hierarchy.isInMainFunction()) {
+    // if we are in the main function just return its hash
+    return std::hash<void *>{}(hierarchy.mainFunction);
+  } else {
+    return pcpo::HashRange(hierarchy.callInstructionsBegin(),
+                           hierarchy.callInstructionsEnd());
+  }
 }
--- src/interprocedural/CallHierarchy.h
+++ src/interprocedural/CallHierarchy.h
@@ -4,17 +4,27 @@
 #include <cassert>
 #include <llvm/IR/Function.h>
 #include <llvm/IR/Instructions.h>
-#include <queue>
 #include <utility>
+#include <vector>
 
 namespace pcpo {
 class CallHierarchy {
   friend std::hash<CallHierarchy>;
 
+  using CallInstructions = std::vector<llvm::CallInst *>;
+
 public:
   explicit CallHierarchy(llvm::Function *mainFunction,
-                         std::deque<llvm::CallInst *> callInsts = {})
-      : mainFunction(mainFunction), callInsts(std::move(callInsts)) {}
+                         CallInstructions callInsts = {},
+                         std::size_t offset = 0U)
+      : mainFunction(mainFunction), callInsts(std::move(callInsts)),
+        offset(offset) {
+    /// Assert against bad offsets
+    assert(offset <= callInsts.size());
+  }
+
+  /// Returns true when we are currently iside the main function
+  bool isInMainFunction() const;
 
   /// Appends the CallInstruction to the current call hierarchy
   CallHierarchy append(llvm::CallInst *callInst) const;
@@ -29,8 +39,18 @@ public:
   bool operator==(CallHierarchy const &other) const;
 
 private:
+  CallInstructions::const_iterator callInstructionsBegin() const;
+  CallInstructions::const_iterator callInstructionsEnd() const;
+
+  /// In case the call hierarchy empty we still require to return the
+  /// current function we are inside and this will be the main function.
   llvm::Function *mainFunction;
-  std::deque<llvm::CallInst *> callInsts;
+  /// A list of all call instructions inside the hierarchy
+  CallInstructions callInsts;
+  /// The offset the call hierarchy starts when we limited it
+  /// to a certain depth. The depth to the end of the callInst vector will
+  /// be taken into account when doing hashing and element comparison.
+  std::size_t offset;
 };
 } // namespace pcpo
 
