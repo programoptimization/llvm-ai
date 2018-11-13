ca87debd27987e2c4f72fe021d3642aa46e46b86
Tim Gymnich <TG908@users.noreply.github.com>
2018-11-13 13:47:46 +0100

    fixed merge error

--- src/fixpoint/branch_conditions.h
+++ src/fixpoint/branch_conditions.h
@@ -30,8 +30,6 @@ public:
 private:
   std::map<BasicBlock *, State> &programPoints;
 
-  std::map<BasicBlock *,
-           std::map<BasicBlock *,
   std::map<BasicBlock * , // Source block
            std::map<BasicBlock *, // Target block
                     std::map<Value *, std::shared_ptr<AbstractDomain>>>>
