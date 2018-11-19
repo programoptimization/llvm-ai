b290a644c5cc9ac1b2094921946ebf8ed72be96a
Philipp Czerner <suyjuris.gi@nicze.de>
2018-11-19 09:12:58 +0100

    It's, like, kind of a little bit, I mean, maybe, working? Also debug output.

---
+++ src/global.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "llvm/Support/raw_ostream.h"
+
+namespace pcpo {
+
+// Higher values mean more debug output.
+//   0: just the result
+//   1: general information about the fixpoint iteration
+//   2: intermediate results in the fixpoint iteration
+//   3: details about the individual operations
+// This could be a compile time constant, but it is not, so that you can set it in your debugger.
+extern int debug_level;
+
+// This is the initial setting
+#define DEBUG_LEVEL 4
+
+// This returns either a stream to stderr or to nowhere, depending on whether we
+// are currently outputting that level.
+inline llvm::raw_ostream& dbgs(int level) {
+    if (level <= DEBUG_LEVEL) {
+        return llvm::errs();
+    } else {
+        return llvm::nulls();
+    }
+}
+
+} /* end of namespace pcpo */
--- src/value_set.cpp
+++ src/value_set.cpp
@@ -2,4 +2,21 @@
 
 namespace pcpo {
 
+char const* get_predicate_name(llvm::CmpInst::Predicate pred) {
+    using Predicate = llvm::CmpInst::Predicate;
+    switch (pred) {
+    case Predicate::ICMP_EQ: return "==";
+    case Predicate::ICMP_NE: return "!=";
+    case Predicate::ICMP_ULE: return "u<=";
+    case Predicate::ICMP_ULT: return "u<";
+    case Predicate::ICMP_UGE: return "u>=";
+    case Predicate::ICMP_UGT: return "u>";
+    case Predicate::ICMP_SLE: return "s<=";
+    case Predicate::ICMP_SLT: return "s<";
+    case Predicate::ICMP_SGE: return "s>=";
+    case Predicate::ICMP_SGT: return "s>";
+    default: return "??";
+    }
+}
+
 } /* end of namespace pcpo */
