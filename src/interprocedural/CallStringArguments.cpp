e92aad35fbe9cb933163f0e0b307d3180ed96bc5
Jan Svoboda <jansvoboda11@gmail.com>
2018-11-20 16:48:36 +0100

    Remove useless files

--- src/interprocedural/CallStringArguments.cpp
+++
@@ -1,6 +0,0 @@
-#include "CallStringArguments.h"
-#include <cassert>
-void pcpo::CallStringArguments::joinIn(const pcpo::CallStringArguments &other) {
-  assert(argDomains.size() == other.argDomains.size());
-  //TODO implement element-wise merge
-}
