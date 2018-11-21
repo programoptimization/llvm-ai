a3fec8b29030e5c16c369230861ce2c80ecb36e1
Philipp <philipp@nicze.de>
2018-11-21 15:45:06 +0100

    Making simple_interval test work again

--- test/run_simple_interval.sh
+++ test/run_simple_interval.sh
@@ -9,7 +9,7 @@ cd $(dirname "$0")
 mkdir -p ../build/test
 
 echo 'Building...'
-g++ SimpleIntervalTest.cpp -I../src -fmax-errors=2 `$llvm_config --cxxflags` -o ../build/test/SimpleIntervalTest `$llvm_config --ldflags` $VSA_LLVM_PATH/lib/llvm-vsa.so `$llvm_config --libs analysis` -lz -lrt -ldl -ltinfo -lpthread -lm 
+g++ simple_interval_test.cpp -I../src -fmax-errors=2 `$llvm_config --cxxflags` -o ../build/test/SimpleIntervalTest `$llvm_config --ldflags` $VSA_LLVM_PATH/lib/llvm-pain.so `$llvm_config --libs analysis` -lz -lrt -ldl -ltinfo -lpthread -lm 
 
 echo 'Running...'
 ../build/test/SimpleIntervalTest
