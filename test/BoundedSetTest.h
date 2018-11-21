d94d56086e067bcfc9d76c61f589c8a08e36080d
Thomas Frank <t.frank@tum.de>
2018-11-21 11:22:33 +0100

    Removed old tests and added a goto sample

--- test/BoundedSetTest.h
+++
@@ -1,6 +0,0 @@
-#ifndef BOUNDED_SET_TEST_H_
-#define BOUNDED_SET_TEST_H_
-namespace pcpo {
-void runBoundedSet();
-} // namespace pcpo
-#endif
--- test/StridedIntervalTest.h
+++
@@ -1,6 +0,0 @@
-#ifndef STRIDED_INTERVAL_TEST_H_
-#define STRIDED_INTERVAL_TEST_H_
-namespace pcpo {
-void runStridedInterval();
-} // namespace pcpo
-#endif
