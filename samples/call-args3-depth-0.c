428a353368390ca7f932743ac0db5d982bd86e81
Denis Blank <denis.blank@tum.de>
2018-11-22 11:14:45 +0100

    Fix the argument evaluation order since it's platform dependent until C++20

--- samples/call-args3-depth-0.c
+++ samples/call-args3-depth-0.c
@@ -8,5 +8,11 @@ int foo(int x, int y) {
 }
 
 int main() {
-  return foo(baz(2), baz(3));
+  // Explicitily evaluate baz(2) before baz(3) since the argument
+  // evaluation order is not defined until C++20 and this will lead
+  // to platform dependent behaviour which produces different ordered
+  // test output.
+  int a = baz(2);
+  int b = baz(3);
+  return foo(a, b);
 }
